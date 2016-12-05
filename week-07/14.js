8290f0739283f1ee4cc3db5a57e4c37736473e0b
Olivia Gardiner <oliv.gardiner@gmail.com>
2016-12-05 15:52:24 +0100

    Strings exercises completed.

---
+++ week-07/14.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var n = 'The result is: ';
+var a = 2;
+var b = 3;
+
+var newVar = n + (a + b);
+console.log(newVar)
+
+// concat the product of a and b to the n string
---
+++ week-07/15.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var o = "pneumonoultramicroscopicsilicovolcanoconiosis";
+console.log(o.length)
+// tell how many letters o has
