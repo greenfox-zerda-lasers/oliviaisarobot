9b66b05929ae5d7808b306d51967e0f616e929fb
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-02-07 12:10:37 +0100

    proper binary search with linear search comparison

--- algorythms/binary_search.js
+++ algorythms/binary_search.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-let search = (sorted, num) => {
+let linearSearch = (sorted, num) => {
   for (let item of sorted) {
     if (item === num) {
       return true
@@ -11,5 +11,36 @@ let search = (sorted, num) => {
   return false
 };
 
-console.log(search([2,3,5,7,9,23,35,55,61,63,64,89], 23));
-console.log(search([2,3,5,7,9,23,35,55,61,63,64,89], 21));
+class binarySearch {
+  constructor (sorted, num) {
+    this.sorted = sorted;
+    this.num = num;
+
+    this.start = 0;
+    this.end = this.sorted.length - 1;
+  }
+
+  find() {
+    while (this.start < this.end) {
+      let mid = Math.floor((this.start + this.end)/2);
+      if (this.sorted[mid] < this.num) {
+        this.start = mid + 1;
+        this.find();
+      } else if (this.sorted[mid] > this.num) {
+        this.end = mid - 1;
+        this.find();
+      } else if (this.sorted[mid] === this.num) {
+        return true
+      }
+    }
+    return false
+  }
+}
+
+// console.log(linearSearch([2,3,5,7,9,23,35,55,61,63,64,89], 23)); // 0.213s
+// console.log(linearSearch([2,3,5,7,9,23,35,55,61,63,64,89], 21)); // 0.264s
+
+let binary = new binarySearch([2,3,5,7,9,23,35,55,61,63,64,89], 23);
+console.log(binary.find()); // 0.178s
+// let binary2 = new binarySearch([2,3,5,7,9,23,35,55,61,63,64,89], 21);
+// console.log(binary2.find()); // 0.259s
